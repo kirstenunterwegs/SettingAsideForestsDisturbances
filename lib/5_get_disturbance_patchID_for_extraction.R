7dc232ab4b77bfcc78f465118ba46c85889c770f
Kirsten Krueger <kirsten.krueger@posteo.de>
2025-03-06 16:53:18 +0100

    add scripts for disturbance extraction and getting patch id

---
+++ lib/5_get_disturbance_patchID_for_extraction.R
@@ -0,0 +1,172 @@
+##################################################################################
+#
+# Get patch ID for disturbances in our sublandscape to extract patch size
+#
+#################################################################################
+
+
+#---------- libraries
+
+library(dplyr)
+library(terra)
+library(tidyr)
+library(raster)
+
+#---------- working directory
+setwd("~/data/") 
+
+
+# -----
+
+folder <- "1o1" # change folder to respective matching setup, for which I want to extract disturbances
+
+# 1o1_forest
+# 1oMany_forest
+
+# ----- 
+
+# --- load data 
+
+
+# !!! decide for the right matching setup !!!
+
+# 1:1 Match
+sublandscapes <- as.data.frame(readRDS("data/processed/matching/match.df.1o1_15perc.rds"))
+
+# 1:Many Match
+#sublandscapes <- as.data.frame(readRDS("data/processed/matching/match.df.1oMany_15perc_n30_filtered.rds"))
+
+
+# load natural disturbances
+dist <- rast("data/processed/disturbances/germany/disturbance_natural_year.tif")
+
+
+sublandscapes <- vect(sublandscapes, geom=c("x", "y"), crs="EPSG:25832") # vectorize sublandscape centroid
+
+# create buffer around sub landscapes
+buffer_sublandscapes <- buffer(sublandscapes, width = 2000)
+
+# mask disturbances with focused area around sublandscapes
+dist.sub <- mask(dist, buffer_sublandscapes)
+writeRaster(dist.sub, paste0("data/processed/disturbances/patch_size/", folder,"/nat_disturbances_year_in_buffer.tif"))
+
+rm(dist, dist.sub, buffer_sublandscapes, sublandscapes) # clear RAM
+
+
+# load subsetted natural disturbance map
+dist.sub <- rast(paste0("data/processed/disturbances/patch_size/", folder,"/nat_disturbances_year_in_buffer.tif"))
+
+# clump annual disturbance patches within buffer
+
+years <- 1986:2020 
+
+
+for (i in 1:length(years)){ # loop takes a while!
+  
+  year <- years[i]
+  print(paste("----------------------", year, "---------------------"))
+  temp.rast <- dist.sub 
+  temp.rast[temp.rast != year] <- NA
+  
+  
+  # natural Disturbance patches
+  natural.patch.map <- terra::patches(terra::mask(dist.sub, temp.rast), directions=4, zeroAsNA=TRUE) # rook-contiguity
+  
+  # write to file
+  writeRaster(natural.patch.map, paste0("data/processed/disturbances/patch_size/", folder,"/years_n/patch_n_", year,".tiff"))
+  
+  # calculate patch size 
+  natural.patch.matrix <- freq(natural.patch.map)
+  
+  # Create a reclassification matrix
+  reclass_matrix <- natural.patch.matrix %>%
+    mutate(count = count * 0.09) %>%  # Convert count to hectares
+    dplyr::select(value, count) %>%   
+    as.matrix()
+  
+  
+  # Reclassify the raster; assign patch size as cell value
+  natural.patch.map_reclass <- classify(natural.patch.map, rcl = reclass_matrix) 
+  
+  
+  # write to file
+  writeRaster(natural.patch.map_reclass, paste0("data/processed/disturbances/patch_size/", folder,"/years_patch/patch_size_", year,".tiff"))
+  
+  rm(temp.rast, natural.patch.map, natural.patch.map_reclass) # free RAM at the end of each iteration
+  
+}
+
+rm(list = ls()) # empty RAM
+
+# Use the patch ID information in the extraction, to calculate the size of the patch within the sublandscape
+# You can use the patch_size layers in teh extraction process to extract the complete patch size, also when it exceeds the sublandscape
+
+# We decided to stick to the patch size WITHIN the landscape (so using the patch_n_ layers)
+
+
+# -----
+
+folder <- "1o1_forest" # change folder to respective matching setup
+
+# 1o1_forest
+# 1oMany_forest
+
+# ----- 
+
+
+# load annual layers and mask them to managed and set aside forest area
+
+# Get a list of all .rds files in the folder
+raster_files <- list.files(paste0("data/processed/disturbances/patch_size/", folder,"/years_patch/"), all.files =TRUE, full.names = TRUE)
+# Remove the first two paths
+raster_files <- raster_files[-c(1, 2)]
+
+# Read all rasters and stack them
+stack <- lapply(raster_files, terra::rast)
+
+# Create a stack from raster list
+patch_size_stack <- rast(stack)
+
+
+# Get a list of all .rds files in the folder
+raster_files <- list.files(paste0("data/processed/disturbances/patch_size/", folder,"/years_n/"), all.files =TRUE, full.names = TRUE)
+# Remove the first two paths
+raster_files <- raster_files[-c(1, 2)]
+
+# Read all rasters and stack them
+stack <- lapply(raster_files, terra::rast)
+
+# Create a stack from raster list
+patch_n_stack <- rast(stack)
+
+
+
+# Rename layers in the stack for clarity
+
+years <- 1986:2020
+names(patch_size_stack) <- as.character(years)
+#writeRaster(patch_size_stack,"processed/disturbances/patch_size/1oMany_forest/patch_size_stack.tif")
+
+names(patch_n_stack) <- as.character(years)
+#writeRaster(patch_n_stack,"processed/disturbances/patch_size/1oMany_forest/patch_n_stack.tif")
+
+
+# separate patch_size_stack layer into set aside/reserve and managed
+
+setaside <- vect( "data/processed/setaside_forest_sites/all_setaside_forest_sites/setaside_forest.gpkg")
+
+Research_patch_size_stack <- crop(mask(patch_size_stack, setaside), setaside)
+Control_patch_size_stack <- terra::mask(patch_size_stack, setaside, inverse = TRUE)
+
+writeRaster(Research_patch_size_stack, paste0("data/processed/disturbances/patch_size/", folder,"/patch_size_stack_reserach.tif"))
+writeRaster(Control_patch_size_stack, paste0("data/processed/disturbances/patch_size/", folder,"/patch_size_stack_control.tif"))
+
+Research_patch_n_stack <- crop(mask(patch_n_stack, setaside), setaside)
+Control_patch_n_stack <- terra::mask(patch_n_stack, setaside, inverse = TRUE)
+
+writeRaster(Research_patch_n_stack, paste0("data/processed/disturbances/patch_size/", folder,"/patch_n_stack_reserach.tif"))
+writeRaster(Control_patch_n_stack, paste0("data/processed/disturbances/patch_size/", folder,"/patch_n_stack_control.tif"))
+
+
+rm(list = ls()) # empty RAM
+
---
+++ lib/6.2_extract_disturbance_metrics_severity_sensitivity.R
@@ -0,0 +1,354 @@
+##################################################################################
+#
+# Extract disturbance information for sublandscapes
+#
+# code for sensitivity analysis of the disturbance severity cutoff point for high 
+# and low disturbance severity
+#
+#################################################################################
+
+
+#---------- libraries
+
+library(dplyr)
+library(terra)
+library(tidyr)
+
+#---------- working directory
+
+setwd("~/data/")
+
+
+# always switch to the respective severity cutoff point! If not specified with 0.7 and 0.9,
+# teh default cutoff (also used in this study) is 0.8
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# --- create natural disturbances stack for disturbance extraction
+
+
+# Load disturbance rasters
+
+disturbance.year.map <- terra::rast("data/processed/disturbances/germany/disturbance_natural_year.tif") # natural disturbances only
+#high.severity.map <- terra::rast("data/processed/disturbances/germany/high.severity.natural_0.7.tif") # natural disturbances only
+high.severity.map <- terra::rast("data/processed/disturbances/germany/high.severity.natural_0.9.tif") # natural disturbances only
+forest.cover <- terra::rast("data/processed/forestcover/forestcover.reclass.tif")
+
+
+# import research shapes
+setaside <- vect( "data/processed/setaside_forest_sites/all_setaside_forest_sites/setaside_forest.gpkg")
+
+# rasterize reserves and NPs (set aside/unmanaged forests)
+
+res <-  res(forest.cover)
+extent <- ext(forest.cover)
+crs <- crs(forest.cover)
+raster <- rast(extent=extent, resolution=res, crs=crs)
+
+site_raster <- rasterize(setaside,raster ,field="newID")
+
+# stack disturbance information and crop to control and reserach area
+
+disturbance_stack <- c(forest.cover, disturbance.year.map, high.severity.map, site_raster)
+names(disturbance_stack) <- c("forest.cover", "disturbance.year", "high.severity","ID")
+
+# Create Research Stack
+
+Research_disturbance_stack <- crop(mask(disturbance_stack, setaside), setaside)
+
+# Create Control Stack
+
+Control_disturbance_stack <- terra::mask(disturbance_stack, setaside, inverse = TRUE)
+
+rm(disturbance_stack, forest.cover, disturbance.year.map, high.severity.map, site_raster) # clear RAM
+
+#writeRaster(Research_disturbance_stack, "data/processed/disturbances/dist_stack_managed_unmanaged/research_stack_nat.agent_0.7_severity.tif")
+#writeRaster(Control_disturbance_stack, "data/processed/disturbances/dist_stack_managed_unmanaged/control_stack_nat.agent_0.7_severity.tif")
+
+writeRaster(Research_disturbance_stack, "data/processed/disturbances/dist_stack_managed_unmanaged/research_stack_nat.agent_0.9_severity.tif")
+writeRaster(Control_disturbance_stack, "data/processed/disturbances/dist_stack_managed_unmanaged/control_stack_nat.agent_0.9_severity.tif")
+
+# -----
+
+folder <- "1o1_forest" # change folder to respective matching setup, for which I want to extract disturbances
+
+# 1o1_forest
+# 1oMany_forest
+
+# ----- 
+
+# load disturbance information stacks
+
+#Research_disturbance_stack <- rast("data/processed/disturbances/dist_stack_managed_unmanaged/research_stack_nat.agent_0.7_severity.tif")
+#Control_disturbance_stack <- rast("data/processed/disturbances/dist_stack_managed_unmanaged/control_stack_nat.agent_0.7_severity.tif")
+
+Research_disturbance_stack <- rast("data/processed/disturbances/dist_stack_managed_unmanaged/research_stack_nat.agent_0.9_severity.tif")
+Control_disturbance_stack <- rast("data/processed/disturbances/dist_stack_managed_unmanaged/control_stack_nat.agent_0.9_severity.tif")
+
+
+# load patch id information for disturbance information extraction
+
+
+stack_id_reserach <- rast(paste0("data/processed/disturbances/patch_size/",folder,"/patch_n_stack_reserach.tif"))
+names(stack_id_reserach) <- paste0("patch.id.",1986:2020)
+stack_id_control <- rast(paste0("data/processed/disturbances/patch_size/",folder,"/patch_n_stack_control.tif"))
+names(stack_id_control) <- paste0("patch.id.",1986:2020)
+
+# stack all disturbance information for extraction
+
+Research_disturbance_stack <- c(Research_disturbance_stack, stack_id_reserach)
+Control_disturbance_stack <- c(Control_disturbance_stack, stack_id_control)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+### --- function to extract disturbance_information for sublandscape
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+get_disturbance_metrics <- function(x, y, Research, window){
+  
+  # define focal window/ extent to extract disturbance information depending on set aside (Research =1) or managed (Reserach = 0) forests
+  
+  if (Research == 1){
+    temp_stack <- Research_disturbance_stack
+    # Define Neighborhood matrix
+    if (window %in% c(15,17,19,21,23,25,27,29,31)){
+      neighborhood_matrix <- matrix(1,nrow=window,ncol=window)
+    } 
+    else{
+      neighborhood_matrix <- matrix(1, nrow = window+1, ncol = window+1)
+      neighborhood_matrix[window+1, ] <- neighborhood_matrix[, window+1] <- 0
+    }
+  } 
+  
+  if (Research == 0){
+    temp_stack <- Control_disturbance_stack
+    # Define Neighborhood matrix
+    neighborhood_matrix <- matrix(1, nrow = window, ncol = window)
+    
+    # Modify the neighborhood_matrix if the window size is not odd sized
+    if (window == 16) {
+      neighborhood_matrix <- matrix(1, nrow = window+1, ncol = window+1)
+      neighborhood_matrix[window+1, ] <- neighborhood_matrix[, window+1] <- 0 
+    }
+  }
+  
+  # identify cell IDs to extract disturbance information from
+  
+  cell  <- terra::cellFromXY(temp_stack, cbind(x, y)) #Testcell: x <- 4604600; y <- 2855400
+  
+  cellnumbers <- as.vector(terra::adjacent(temp_stack, cell, directions = neighborhood_matrix))
+  disturbance_information <- as.data.frame(temp_stack[cellnumbers, drop = FALSE], xy=TRUE)
+  
+  cells_in_sublandscape <- nrow(dplyr::filter(disturbance_information, !is.na(forest.cover))) #ID / forest.cover
+  
+  
+  #get numbers of disturbed cells per year
+  
+  cells.per.year <- disturbance_information %>%
+    dplyr::filter(complete.cases(disturbance.year))%>%
+    dplyr::group_by(disturbance.year) %>% 
+    dplyr::summarise(ncells.per.year = n()) %>%
+    dplyr::right_join(data.frame(disturbance.year = 1986:2020,
+                                 default.cells.per.year = 0), by = c("disturbance.year")) %>% 
+    dplyr::mutate(ncells.per.year = pmax(ncells.per.year, default.cells.per.year, na.rm=TRUE)) %>%
+    dplyr::arrange(disturbance.year) %>%
+    dplyr::select(-c(default.cells.per.year)) %>% 
+    tidyr::spread(key=disturbance.year, value = ncells.per.year) %>%
+    as.matrix()
+  
+  
+  #get number of severe disturbed cells per year
+  
+  severity.cells.per.year <- disturbance_information %>%
+    dplyr::filter(high.severity == 1) %>%
+    dplyr::group_by(disturbance.year) %>%
+    dplyr::summarise(ncells.per.year = n()) %>%
+    dplyr::right_join(data.frame(disturbance.year = 1986:2020,
+                                 default.cells.per.year = 0), by = c("disturbance.year")) %>%
+    dplyr::mutate(ncells.per.year = pmax(ncells.per.year, default.cells.per.year, na.rm=TRUE)) %>%
+    dplyr::arrange(disturbance.year) %>%
+    dplyr::select(-c(default.cells.per.year)) %>%
+    tidyr::spread(key=disturbance.year, value = ncells.per.year) %>%
+    as.matrix()
+  
+  
+  # get disturbance patch sizes (patches extend only within landscape! - crop disturbance patches to landscapes)
+  
+  patch.size <- disturbance_information %>%
+    dplyr::filter(complete.cases(disturbance.year))%>%
+    pivot_longer(cols = starts_with("patch.id."),
+                 names_to = "year",
+                 values_to = "patch_id") %>%
+    filter(!is.na(patch_id)) %>% # Remove NA patch IDs
+    count(year, patch_id) %>% # Count occurrences of each patch ID per year
+    mutate(area_ha = n * 0.09) %>% # Convert counts to hectares
+    select(-n)
+  
+  # maximum patch size in landscape
+  max.patch <- patch.size %>%
+    summarise(max = if (all(is.na(area_ha))) { 0 } 
+              else { max(area_ha, na.rm = TRUE)}) %>%
+    as.matrix()
+  
+  # (area-) weighted mean patch size (following Li & Archer 1997)
+  wm_patch_size <- patch.size %>%
+    count(area_ha) %>%
+    mutate(n_s = n * area_ha) %>%
+    mutate(n_s2 = n * area_ha^2) %>%
+    summarise( sum_n_s2 = sum(n_s2),
+               sum_n_s = sum(n_s)) %>%
+    mutate(wmps = if (sum_n_s == 0) 0 else sum_n_s2 / sum_n_s) %>%
+    select(wmps) %>% 
+    as.matrix()
+  
+  # get density (n of disturbances patches per landscape)
+  # Counting the number of unique values in patch.id. columns for each year
+  
+  density <- disturbance_information %>%
+    select(starts_with("patch.id.")) %>%
+    summarise(across(everything(), ~length(unique(na.omit(.))))) %>%
+    sum(.)%>%
+    as.matrix()
+  
+  # get frequency (n of disturbance events per landscape)
+  # Counting the number of unique values in freq columns for each year
+  
+  frequency <- disturbance_information %>%
+    select(disturbance.year) %>%
+    filter(disturbance.year > 0) %>%
+    summarise(n_events = length(unique(disturbance.year))) %>%
+    as.matrix()
+  
+  
+  
+  return(list("ncells" = cells_in_sublandscape,
+              "cells.per.year" = cells.per.year,
+              "severity.cells.per.year" = severity.cells.per.year,
+              "max.patch" = max.patch,
+              "mean.patch" = wm_patch_size,
+              "density" = density,
+              "frequency" = frequency))
+}
+
+options(dplyr.summarise.inform = FALSE)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+##### --------------------- disturbance extraction -------------------- #####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# load the respective matching data frame
+
+if(folder == "1o1_forest") {
+  
+  export_df <- readRDS("data/processed/matching/match.df.1o1_15perc.rds")
+  
+} else if (folder == "1oMany_forest") {
+  
+  export_df <- readRDS("data/processed/matching/match.df.1oMany_15perc_n30_filtered.rds")
+  
+}
+
+
+# --- assign the window size  and fcover of the research site to attain window for extracting control site disturbance info
+
+# assign fcover of reserach sublandscape to control sublandscape (to compare extracted fcover in control site to reserach site)
+export_df <- export_df %>%
+  group_by(subclass) %>%
+  #mutate(fcover = fcover[Research == 1])
+  mutate(fcover = fcover[which(Research == 1)[1]]) # to deal with pseudo replication in 1:Many sampling design of unmanaged sublandscapes
+
+# decide on neighboorhood-matrix size depending on forestcover in Reserach site, to guarantee max. 2 ha difference
+export_df$window <- ifelse(export_df$Research == 0 & export_df$fcover > 242 & export_df$fcover < 270, 16,
+                                                ifelse(export_df$Research == 0 & export_df$fcover > 270, 17,
+                                                  ifelse(export_df$Research == 0 & export_df$fcover <= 242, 15, export_df$window)))
+                           
+
+
+
+# extraction
+
+pb <- dplyr::progress_estimated(nrow(export_df))
+for (i in 1:nrow(export_df)){
+  sublandscape <- export_df[i,]
+ disturbance_informations <- get_disturbance_metrics(x = sublandscape$x, y = sublandscape$y, Research = sublandscape$Research, window = sublandscape$window)
+  if ((disturbance_informations$ncells - sublandscape$fcover) > 22 | (disturbance_informations$ncells - sublandscape$fcover) < -22) {print(paste("more than 2 ha difference in forest cover in ", i))} # 22*0.09 = 2.07 ha
+  
+  export_df[i, paste0("disturbed.cells.", 1986:2020)] <- disturbance_informations$cells.per.year 
+  export_df[i, paste0("severe.disturbed.cells.", 1986:2020)] <- disturbance_informations$severity.cells.per.year
+  export_df[i, "density"] <- disturbance_informations$density
+  export_df[i, "max.patch"] <- disturbance_informations$max.patch
+  export_df[i, "mean.patch"] <- disturbance_informations$mean.patch
+  export_df[i, "frequency"] <- disturbance_informations$frequency
+  export_df[i, "n.fcover"] <- disturbance_informations$ncells
+
+  pb$tick()$print()
+  
+}
+
+# round weighted mean patch size
+export_df$mean.patch <- round(export_df$mean.patch, 2)
+
+head(export_df)
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# add climatic data to check whether site conditions properly reflect temperature and precipitation similarities
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# unzip climate data (downloaded multi-annual averages from https://opendata.dwd.de/climate_environment/)
+
+
+# precipitation
+
+# ras_path <- "raw/climate/grids_germany_multi_annual_precipitation_1991-2020_17.asc.gz"
+# 
+# R.utils::gunzip(ras_path, remove = FALSE)
+# 
+# # average temperature
+# 
+# ras_path <- "raw/climate/grids_germany_multi_annual_air_temp_mean_1991_2020_17.asc.gz"
+# 
+# R.utils::gunzip(ras_path, remove = FALSE)
+
+# 
+# prec <- rast("raw/climate/grids_germany_multi_annual_precipitation_1991-2020_17.asc")
+# temp <- rast("raw/climate/grids_germany_multi_annual_air_temp_mean_1991_2020_17.asc")
+
+# # assign crs from documentation of dwd data
+# crs(prec) <- "EPSG:31467"
+# crs(temp) <- "EPSG:31467"
+# 
+# # reproject raster data for climate data extraction
+# 
+# prec <- project(prec, Research_disturbance_stack)
+# temp <- project(temp, Research_disturbance_stack)
+# 
+# writeRaster(prec,"raw/climate/multi_annual_precipitation_1991-2020_17_25832.tiff" )
+# writeRaster(temp,"raw/climate/multi_annual_air_temp_mean_1991_2020_17_25832.tiff" )
+
+
+prec <- rast("data/raw/climate/multi_annual_precipitation_1991-2020_17_25832.tiff")
+temp <- rast("data/raw/climate/multi_annual_air_temp_mean_1991_2020_17_25832.tiff" )
+
+# create points from sublandscape df for raster extraction
+
+subland.points <- vect(as.data.frame(export_df), geom=c("x", "y"), crs ="EPSG:25832" ) 
+
+# extract climate metrics
+
+subland.points <- terra::extract(prec, subland.points, bind=TRUE, method= "simple")
+subland.points <- terra::extract(temp, subland.points, bind=TRUE, method= "simple")
+
+dist.climate.df <- as.data.frame(subland.points, geom="XY")
+
+# rename climate metrics
+
+dist.climate.df <- dist.climate.df %>%
+  rename(mean_prec91_20 = "grids_germany_multi_annual_precipitation_1991-2020_17",
+         mean_temp91_20 = "grids_germany_multi_annual_air_temp_mean_1991_2020_17")
+
+
+#saveRDS(dist.climate.df, paste0("data/processed/dist.extraction/",folder,"/match.df_climate_dist_0.7_severity.rds"))
+saveRDS(dist.climate.df, paste0("data/processed/dist.extraction/",folder,"/match.df_climate_dist_0.9_severity.rds"))
